66cd1cc230a1299fa5976aec044820db75917334
limjiyoon <luinpark@gmail.com>
2023-02-01 05:29:16 +0900

    Add solution using bit - 1626

--- leetcode/1626_best_team_with_no_conflicts.py
+++ leetcode/1626_best_team_with_no_conflicts.py
@@ -40,3 +40,39 @@ class Solution:
                     max_seq[cur_idx] < max_seq[prev_idx] + cur_value):
                     max_seq[cur_idx] = max_seq[prev_idx] + cur_value
         return max(max_seq)
+
+    def best_team_score_v3(self, scores: list[int], ages: list[int]) -> int:
+        """Using binary indexed tree (modified other's solution)
+
+        Notes:
+            Time compleixty: O(NlogN + NlogK)
+            Space complexity: O(N + K)
+        Reference:
+            https://leetcode.com/problems/best-team-with-no-conflicts/solutions/2886659/best-team-with-no-conflicts/
+        """
+        def query(bit: list[int], idx: int):
+            """Query maximum sum before idx."""
+            max_score = 0  # Problem constraints: 1 <= score <= 10^6
+            while idx > 0:
+                max_score = max(max_score, bit[idx])
+                idx -= (idx & (-idx))
+            return max_score
+
+        def update(bit: list[int], idx: int, value: int) -> None:
+            """Add current value to next nodes."""
+            bit_len = len(bit)
+            while idx < bit_len:
+                bit[idx] = max(bit[idx], value)
+                idx += (idx & (-idx))
+
+        scores, ages = zip(*sorted(zip(scores, ages)))
+        unique_ages = list(set(ages))
+        age_to_idx = {age: idx for idx, age in enumerate(sorted(unique_ages), 1)}
+        binary_indexed_tree = [0] * (len(age_to_idx) + 1)
+        best_score = 0
+        for age, score in zip(ages, scores):
+            idx = age_to_idx[age]
+            cur_best_score = score + query(binary_indexed_tree, idx)
+            update(binary_indexed_tree, idx, cur_best_score)
+            best_score = max(best_score, cur_best_score)
+        return best_score
