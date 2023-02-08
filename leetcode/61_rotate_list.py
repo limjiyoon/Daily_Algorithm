eee1497359b5cda53617ec54baaf854ac7dd028e
limjiyoon <luinpark@gmail.com>
2023-02-09 00:01:53 +0900

    Solve 61_rotate_list.py

--- leetcode/61_rotate_list.py
+++ leetcode/61_rotate_list.py
@@ -7,7 +7,8 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    def rotate_right_v1(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        """Over time requirement if k is too large."""
         if not head or not head.next or k == 0:
             return head
 
@@ -17,6 +18,34 @@ class Solution:
 
         if mid == head:
             return head
+
+        last = head
+        while mid.next:
+            mid = mid.next
+            last = last.next
+
+        mid.next = head
+        head, last.next = last.next, None
+        return head
+
+    def rotate_right_v2(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        """Solve with two pointer method and pre-calculate length."""
+        if not head or not head.next or k == 0:
+            return head
+
+        length = 1
+        cur = head
+        while cur.next:
+            cur = cur.next
+            length += 1
+        k = k % length
+
+        if k == 0:
+            return head
+
+        mid = head
+        for _ in range(k):
+            mid = mid.next
 
         last = head
         while mid.next:
