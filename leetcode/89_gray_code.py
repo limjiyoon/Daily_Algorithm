35cf2c00c3ed584c21a2033a093040ea8aed709c
limjiyoon <luinpark@gmail.com>
2023-02-12 08:17:23 +0900

    Solve 89. Gray Code by math formula.

--- leetcode/89_gray_code.py
+++ leetcode/89_gray_code.py
@@ -1,13 +1,17 @@
 class Solution:
-    def grayCode(self, n: int) -> list[int]:
+    def gray_code_v1(self, n: int) -> list[int]:
         """Solve by recursion."""
 
-        def dp(num: int, n: int):
+        def recursion(num: int, n: int):
             if n < 0:
                 return num
-            num = yield from dp(num, n - 1)
+            num = yield from recursion(num, n - 1)
             num ^= (2 ** n)
             yield num
-            num = yield from dp(num, n - 1)
+            num = yield from recursion(num, n - 1)
             return num
-        return [0] + list(dp(0, n - 1))
+        return [0] + list(recursion(0, n - 1))
+
+    def gray_code_v2(self, n: int) -> list[int]:
+        """Solve by math analysis."""
+        return [num ^ (num >> 1) for num in range(0, 2 ** n)]
